6900bc03314be51256363bf1556e52b10e391e31
JBladejs <JBladejs@gmail.com>
2018-11-21 17:27:31 +0100

    added zad04

---
+++ zad04/src/main/java/com/bladejs/zad04/domain/CEO.java
@@ -0,0 +1,6 @@
+package com.bladejs.zad04.domain;
+
+public class CEO {
+    private String firstName;
+    private String lastName;
+}
---
+++ zad04/src/main/java/com/bladejs/zad04/domain/Producer.java
@@ -0,0 +1,9 @@
+package com.bladejs.zad04.domain;
+
+public class Producer {
+    private String name;
+    private String country;
+    private CEO ceo;
+
+
+}
