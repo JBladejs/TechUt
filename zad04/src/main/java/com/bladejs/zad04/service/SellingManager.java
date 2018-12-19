6aae315d85612c34076d8ce08c502932aef6275b
JBladejs <JBladejs@gmail.com>
2018-12-19 17:28:31 +0100

    additions to interface

--- zad04/src/main/java/com/bladejs/zad04/service/SellingManager.java
+++ zad04/src/main/java/com/bladejs/zad04/service/SellingManager.java
@@ -11,9 +11,15 @@ public interface SellingManager {
 
     void addCeo(CEO ceo);
     List<CEO> getAllCEOs();
-    CEO findCEObyProducer(Producer producer);
+    CEO findCEOByProducer(Producer producer);
 
     void addNewGraphicsCard(GraphicsCard gpu);
     List<GraphicsCard> getAvailableGraphicsCards();
     void sellGraphicsCard(Client client, GraphicsCard gpu);
+    GraphicsCard findGraphicsCardById(Long id);
+
+    List<GraphicsCard> getOwnnedGraphicsCards(Client client);
+    void sellGraphicsCard(Long clientId, Long graphicsCardID);
+
+
 }
