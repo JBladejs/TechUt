7f1f67d3419eb84a1cdb3ae78340ddcea4963b45
JBladejs <JBladejs@gmail.com>
2019-01-16 03:17:03 +0100

    added ClientCrudInterface

--- zad04/src/main/java/com/bladejs/shdemo/domain/Client.java
+++ zad04/src/main/java/com/bladejs/shdemo/domain/Client.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 @Entity
 @NamedQueries({
-        @NamedQuery(name = "client.all", query = "Select c from Client c")
+        @NamedQuery(name = "client.byLogin", query = "Select c from Client c where c.login = :login")
 })
 public class Client {
 
@@ -14,10 +14,10 @@ public class Client {
     private long id;
     @Column(unique = true, nullable = false)
     private String login;
-    @Column(nullable = false)
-    private String password;
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<GraphicsCard> graphicsCards;
+    private String firstName="unkown";
+    private String lastName="unkown";
 
     public long getId() {
         return id;
@@ -33,17 +33,24 @@ public class Client {
         this.login = login;
     }
 
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public List<GraphicsCard> getGraphicsCards() {
         return graphicsCards;
     }
     public void setGraphicsCards(List<GraphicsCard> graphicsCards) {
         this.graphicsCards = graphicsCards;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- zad04/src/main/java/com/bladejs/shdemo/service/SellingManager.java
+++ zad04/src/main/java/com/bladejs/shdemo/service/SellingManager.java
@@ -1,7 +1,14 @@
 package com.bladejs.shdemo.service;
 
+import com.bladejs.shdemo.domain.Client;
+
 import java.util.List;
 
 public interface SellingManager {
+
+    void addClient(Client client);
+    List<Client> getAllClients();
+    void deleteClient(Client client);
+    Client findClientByLogin(String login);
 
 }
